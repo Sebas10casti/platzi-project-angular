67a148d57f51309759c1667ee422beedd5084f64
scastiblanco-lealco <scastiblanco@leal.co>
2023-02-06 15:42:53 -0500

    Terminando el curso de programacion modular y lazy lloading

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/my-store-api'),
+      dir: require('path').join(__dirname, './coverage/my-store-router'),
       subdir: '.',
       reporters: [
         { type: 'html' },
